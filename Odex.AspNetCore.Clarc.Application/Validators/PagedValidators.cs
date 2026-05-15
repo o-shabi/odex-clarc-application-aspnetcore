81c6cf9571bea02a65587e4c0b2599fb554905c4
Asen O'Shabi <shabiasin@gmail.com>
2026-05-15 15:38:54 +0330

    0.2.0 - Use BaseValidator

--- Odex.AspNetCore.Clarc.Application/Validators/PagedValidators.cs
+++ Odex.AspNetCore.Clarc.Application/Validators/PagedValidators.cs
@@ -7,7 +7,7 @@ namespace Odex.AspNetCore.Clarc.Application.Validators;
 /// FluentValidation rules for <see cref="PagedQuery{TResponse}"/> (page ≥ 1, page size 1–512).
 /// </summary>
 /// <typeparam name="T">The query response type (unused for rules; required for <see cref="PagedQuery{TResponse}"/>).</typeparam>
-public class PagedValidator<T> : AbstractValidator<PagedQuery<T>>
+public class PagedValidator<T> : BaseValidator<PagedQuery<T>>
 {
     /// <summary>
     /// Initializes a new instance with default pagination bounds.
